48cf35f8e70ce04f8ab1bf1246c4362201de89dd
jadkins-me <jadkins21@protonmail.com>
2024-12-24 16:28:33 +0000

    add: new client error message

--- src/client/autonomi_messages.py
+++ src/client/autonomi_messages.py
@@ -20,6 +20,7 @@ _error_client = "error:client"
 _error_dataloss = "error:dataloss"
 
 _error_messages = {
+    "cost error: not enough node quotes" : _error_network,
     "failed to fetch data from address" : _error_dataloss,
     "could not connect to enough peers in time": _error_network,
     "failed to connect to the network": _error_network,
